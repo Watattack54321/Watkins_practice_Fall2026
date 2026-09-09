3f72518f285cce6601f97484d2c35527ae1b469d
Student <Student@A414-21>
2026-09-09 11:33:57 -0400

    Add project files.

---
+++ Watkins_practice_Fall2026/Form1.cs
@@ -0,0 +1,20 @@
+namespace Watkins_practice_Fall2026
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
